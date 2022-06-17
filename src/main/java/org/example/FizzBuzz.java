dbe2cc6bf9f9df4552957617f68ffdf0831651ad
Daniel <daniel.villahermosa@codurance.com>
2022-06-17 14:22:13 +0200

    Remove App and refactor

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -3,7 +3,9 @@ package org.example;
 public class FizzBuzz {
     public static String of(int number) {
         String result = "";
-        if(number % 3 == 0) result =  "Fizz";
+        if(number % 3 == 0) {
+            result =  "Fizz";
+        }
         if(number % 5 == 0) {
             result +=  "Buzz";
         }
